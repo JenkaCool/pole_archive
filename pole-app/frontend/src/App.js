3fedaec3b206a493f3cef439b31547e8b7c80dd8
JenkaCool <cool.diatel@yandex.ru>
2023-03-17 04:35:21 +0300

    components and main page

--- pole-app/frontend/src/App.js
+++ pole-app/frontend/src/App.js
@@ -15,12 +15,30 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {!data ? "Loading..." : data}
-        </p>
-      </header>
+        <header className="App-header">
+            <div id="user-bar" className="in-one-role">
+                <button><a class="button">Вход</a></button>
+                <button><a class="button">Регистрация</a></button>
+                <button><a class="button">Профиль</a></button>
+            </div>
+            <br/>
+            <h3>Список записей о польских ссыльных в Олонецкой губернии</h3>
+            <div id='navigation'>
+                <button><a class="button">Список записей</a></button>
+                <button><a class="button">Список документов</a></button>
+            </div>
+        </header>
+        <div id='tools'>
+            <button><a class="button-add">Добавить новую запись</a></button>
+        </div>
+        <div class="big-table">;
+            <table>
+                <thead>
+                    <tr></tr>
+                </thead>
+                <tbody><tr></tr></tbody>
+            </table>
+        </div>
     </div>
   );
 }
