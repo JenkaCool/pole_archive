89718328bdb437b8745cf4ea96b0f7814d44543e
JenkaCool <Cool.diatel@yandex.ru>
2023-06-01 15:59:45 +0300

    rollback remove paths

--- pole-app/frontend/src/index.js
+++ pole-app/frontend/src/index.js
@@ -27,7 +27,7 @@ import PageNotFound from './Components/PageNotFound';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/exiles">
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}>
 
